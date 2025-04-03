ee60e20f87605dcdd5375c0d53d94d17b1fd84f4
AzamatG17 <128783946+AzamatG17@users.noreply.github.com>
2025-04-03 23:46:00 +0500

    implement configuration for entities

---
+++ SmartBudget.Api/SmartBudget.Api/Persistence/Configurations/TransactionConfiguration.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using SmartBudget.Api.Models.Entities;
+
+namespace SmartBudget.Api.Persistence.Configurations;
+
+public class TransactionConfiguration : IEntityTypeConfiguration<Transaction>
+{
+    public void Configure(EntityTypeBuilder<Transaction> builder)
+    {
+        builder.ToTable(nameof(Transaction));
+        builder.HasKey(t => t.Id);
+
+        builder.Property(t => t.DateAdded)
+            .IsRequired();
+
+        builder.Property(t => t.Amount)
+            .IsRequired()
+            .HasColumnType("decimal(18,2)");
+
+        builder.Property(t => t.Comment);
+
+        builder.HasOne(t => t.Category)
+            .WithMany(c => c.Transactions)
+            .HasForeignKey(t => t.CategoryId)
+            .OnDelete(DeleteBehavior.Cascade);
+    }
+}
