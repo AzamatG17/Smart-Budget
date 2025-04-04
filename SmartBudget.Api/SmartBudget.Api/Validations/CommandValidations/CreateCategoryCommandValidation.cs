1c628131be85ca29e6068c39079b568f9777898c
Azamat Giyosov <azamatgiasov04@gmail.com>
2025-04-04 17:42:12 +0500

    Add Validations for Commands

---
+++ SmartBudget.Api/SmartBudget.Api/Validations/CommandValidations/CreateCategoryCommandValidation.cs
@@ -0,0 +1,19 @@
+﻿using FluentValidation;
+using SmartBudget.Api.Features.Commands.CategoryQueries;
+
+namespace SmartBudget.Api.Validations.CommandValidations;
+
+public class CreateCategoryCommandValidation : AbstractValidator<CreateCategoryCommand>
+{
+    public CreateCategoryCommandValidation()
+    {
+        RuleFor(x => x.Name)
+            .NotEmpty()
+            .WithMessage("Name is required.")
+            .MaximumLength(255)
+            .WithMessage("Name must not exceed 255 characters.");
+        RuleFor(x => x.TransactionType)
+            .IsInEnum()
+            .WithMessage("TransactionType must be either Income or Expense.");
+    }
+}
---
+++ SmartBudget.Api/SmartBudget.Api/Validations/CommandValidations/UpdateTransactionCommandValidation.cs
@@ -0,0 +1,27 @@
+﻿using FluentValidation;
+using SmartBudget.Api.Features.Commands.TransactionCommands;
+
+namespace SmartBudget.Api.Validations.CommandValidations;
+
+public class UpdateTransactionCommandValidation : AbstractValidator<UpdateTransactionCommand>
+{
+    public UpdateTransactionCommandValidation()
+    {
+        RuleFor(x => x.Id)
+            .NotEmpty()
+            .WithMessage("Id is required.");
+        RuleFor(x => x.Amount)
+            .NotEmpty()
+            .WithMessage("Amount is required.")
+            .GreaterThan(0)
+            .WithMessage("Amount must be greater than 0.");
+        RuleFor(x => x.Date)
+            .NotEmpty()
+            .WithMessage("Date is required.")
+            .LessThanOrEqualTo(DateTime.Now)
+            .WithMessage("Date must be less than or equal to today.");
+        RuleFor(x => x.CategoryId)
+            .NotEmpty()
+            .WithMessage("CategoryId is required.");
+    }
+}
