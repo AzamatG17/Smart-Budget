3ea8845e8ae87bca07099c27bc9b4dac9326c055
Azamat Giyosov <azamatgiasov04@gmail.com>
2025-04-04 17:44:09 +0500

    Register Balidation and AutoMapper in DI

--- SmartBudget.Api/SmartBudget.Api/Extentions/DependencyInjection.cs
+++ SmartBudget.Api/SmartBudget.Api/Extentions/DependencyInjection.cs
@@ -1,6 +1,10 @@
 ﻿using SmartBudget.Api.Interfaces;
 using SmartBudget.Api.Persistence;
 using Microsoft.EntityFrameworkCore;
+using FluentValidation;
+using SmartBudget.Api.Validations.CommandValidations;
+using FluentValidation.AspNetCore;
+using SmartBudget.Api.Mappings;
 
 namespace SmartBudget.Api.Extentions;
 
@@ -9,6 +13,10 @@ public static class DependencyInjection
     public static IServiceCollection RegisterApi(this IServiceCollection services, IConfiguration configuration)
     {
         AddDatabaseContext(services, configuration);
+
+        services.AddAutoMapper(typeof(CategoryMapping).Assembly);
+        services.AddValidatorsFromAssemblyContaining<CreateCategoryCommandValidation>();
+        services.AddFluentValidationAutoValidation();
 
         return services;
     }
