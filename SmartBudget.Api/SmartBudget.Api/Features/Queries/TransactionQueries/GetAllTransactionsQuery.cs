bef7d7757127b65b0c8ee3af27d04fa7d3feff5e
Azamat Giyosov <azamatgiasov04@gmail.com>
2025-04-04 17:41:21 +0500

    Add Queries

---
+++ SmartBudget.Api/SmartBudget.Api/Features/Queries/TransactionQueries/GetAllTransactionsQuery.cs
@@ -0,0 +1,6 @@
+﻿using MediatR;
+using SmartBudget.Api.Models.DTOs;
+
+namespace SmartBudget.Api.Features.Queries.TransactionQueries;
+
+public record GetAllTransactionsQuery : IRequest<List<TransactionDto>>;
---
+++ SmartBudget.Api/SmartBudget.Api/Features/Queries/TransactionQueries/GetTransactionByIdQuery.cs
@@ -0,0 +1,6 @@
+﻿using MediatR;
+using SmartBudget.Api.Models.DTOs;
+
+namespace SmartBudget.Api.Features.Queries.TransactionQueries;
+
+public record GetTransactionByIdQuery(int Id) : IRequest<TransactionDto>;
