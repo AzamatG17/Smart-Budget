b99fec309ceaea0d31e3e437a0838851448e8e70
Azamat Giyosov <azamatgiasov04@gmail.com>
2025-04-04 17:41:51 +0500

    Implement Handlers

---
+++ SmartBudget.Api/SmartBudget.Api/Features/Handlers/CategoryHandlers/DeleteCategoryHandler.cs
@@ -0,0 +1,27 @@
+﻿using MediatR;
+using Microsoft.EntityFrameworkCore;
+using SmartBudget.Api.Exceptions;
+using SmartBudget.Api.Features.Commands.CategoryCommands;
+using SmartBudget.Api.Interfaces;
+
+namespace SmartBudget.Api.Features.Handlers.CategoryHandlers;
+
+public class DeleteCategoryHandler(IApplicationDbContext context) : IRequestHandler<DeleteCategoryCommand, bool>
+{
+    private readonly IApplicationDbContext _context = context ?? throw new ArgumentNullException(nameof(context));
+
+    public async Task<bool> Handle(DeleteCategoryCommand request, CancellationToken cancellationToken)
+    {
+        ArgumentNullException.ThrowIfNull(request);
+
+        var category = await _context.Categories
+            .AsNoTracking()
+            .FirstOrDefaultAsync(c => c.Id == request.Id, cancellationToken)
+            ?? throw new EntityNotFoundException($"Category with id {request.Id} not found.");
+
+        _context.Categories.Remove(category);
+        await _context.SaveChangesAsync(cancellationToken);
+
+        return true;
+    }
+}
---
+++ SmartBudget.Api/SmartBudget.Api/Features/Handlers/TransactionHandlers/GetTransactionByIdHandler.cs
@@ -0,0 +1,27 @@
+﻿using AutoMapper;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+using SmartBudget.Api.Exceptions;
+using SmartBudget.Api.Features.Queries.TransactionQueries;
+using SmartBudget.Api.Interfaces;
+using SmartBudget.Api.Models.DTOs;
+
+namespace SmartBudget.Api.Features.Handlers.TransactionHandlers;
+
+public class GetTransactionByIdHandler(IApplicationDbContext context, IMapper mapper) : IRequestHandler<GetTransactionByIdQuery, TransactionDto>
+{
+    private readonly IApplicationDbContext _context = context ?? throw new ArgumentNullException(nameof(context));
+    private readonly IMapper _mapper = mapper ?? throw new ArgumentNullException(nameof(mapper));
+
+    public async Task<TransactionDto> Handle(GetTransactionByIdQuery request, CancellationToken cancellationToken)
+    {
+        ArgumentNullException.ThrowIfNull(request);
+
+        var result = await _context.Transactions
+            .AsNoTracking()
+            .FirstOrDefaultAsync(t => t.Id == request.Id, cancellationToken)
+            ?? throw new EntityNotFoundException($"Transaction with id {request.Id} not found.");
+
+        return _mapper.Map<TransactionDto>(result);
+    }
+}
