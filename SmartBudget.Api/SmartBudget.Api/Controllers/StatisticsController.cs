f2f7974b1b38257bcce1778a642bf1c59a0d2e7f
AzamatG17 <128783946+AzamatG17@users.noreply.github.com>
2025-04-06 23:23:23 +0500

    Create StatisticsController

---
+++ SmartBudget.Api/SmartBudget.Api/Controllers/StatisticsController.cs
@@ -0,0 +1,19 @@
+﻿using MediatR;
+using Microsoft.AspNetCore.Mvc;
+using SmartBudget.Api.Features.Queries.StatisticsQueries;
+
+namespace SmartBudget.Api.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+public class StatisticsController(IMediator mediator) : ControllerBase
+{
+    private readonly IMediator _mediator = mediator ?? throw new ArgumentNullException(nameof(mediator));
+
+    [HttpGet]
+    public async Task<IActionResult> GetStatistics([FromQuery] GetStatisticsQuery query)
+    {
+        var result = await _mediator.Send(query);
+        return Ok(result);
+    }
+}
