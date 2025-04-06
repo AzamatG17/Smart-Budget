edc0169476dd1a55e95904b89eae04652ec81f96
AzamatG17 <128783946+AzamatG17@users.noreply.github.com>
2025-04-06 23:23:07 +0500

    add DTOs

---
+++ SmartBudget.Api/SmartBudget.Api/Models/DTOs/StatisticsDto.cs
@@ -0,0 +1,10 @@
+﻿using SmartBudget.Api.Models.Enums;
+
+namespace SmartBudget.Api.Models.DTOs;
+
+public record StatisticsDto(
+    int? Year,
+    int? Month,
+    decimal? TotalAmount,
+    TransactionType? TransactionType
+    );
