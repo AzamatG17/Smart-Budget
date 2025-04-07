bbf6e2902c10badd062d2d527663a4484d78d491
Azamat Giyosov <azamatgiasov04@gmail.com>
2025-04-07 10:27:04 +0500

    Create Validation for GetStatisticsQuery

---
+++ SmartBudget.Api/SmartBudget.Api/Validations/QueryValidations/GetStatisticsQueryValidation.cs
@@ -0,0 +1,26 @@
+﻿using FluentValidation;
+using SmartBudget.Api.Features.Queries.StatisticsQueries;
+
+namespace SmartBudget.Api.Validations.QueryValidations;
+
+public class GetStatisticsQueryValidation : AbstractValidator<GetStatisticsQuery>
+{
+    public GetStatisticsQueryValidation()
+    {
+        RuleFor(x => x.Year)
+            .NotEmpty()
+            .WithMessage("Year is required.");
+
+        RuleFor(x => x.Month)
+            .NotEmpty()
+            .WithMessage("Month is required.")
+            .InclusiveBetween(1, 12)
+            .WithMessage("Month must be between 1 and 12.");
+
+        RuleFor(x => x.TransactionType)
+            .NotEmpty()
+            .WithMessage("TransactionType is required.")
+            .IsInEnum()
+            .WithMessage("TransactionType must be a valid enum value.");
+    }
+}
